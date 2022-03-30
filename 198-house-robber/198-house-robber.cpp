5303d5765412a9d6cef8d78181eb8595b95393c2
deep-2008 <63959533+deep-2008@users.noreply.github.com>
2022-03-30 11:03:58 +0530

    Time: 3 ms (52.69%), Space: 7.7 MB (76.02%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+       int n=nums.size();
+        int t[n];
+        if(n==1)
+            return nums[0];
+      t[0]=nums[0];
+        t[1]=max(nums[0],nums[1]);
+        
+        for(int i=2;i<n;i++){
+            t[i] = max(t[i-1],t[i-2]+nums[i]);
+        }
+        return t[n-1];
+    }
+};
