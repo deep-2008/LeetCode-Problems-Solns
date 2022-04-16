7b6e01c536c3df8a0076f11002b83ae0336f9b15
deep-2008 <63959533+deep-2008@users.noreply.github.com>
2022-04-16 09:26:35 +0530

    Time: 3 ms (39.82%), Space: 6.9 MB (11.64%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+     int n=t.size();
+        int m=s.size();
+        int dp[n+1][m+1];
+        
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                if(i==0 || j==0)
+                    dp[i][j]=0;
+                else if(t[i-1]==s[j-1])
+                    dp[i][j] = dp[i-1][j-1]+1;
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        
+        if(m==dp[n][m])
+            return true;
+        return false;
+    }
+};
