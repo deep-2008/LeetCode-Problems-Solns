b8d2022747a659e017bc804ab4a912b8535c912d
deep-2008 <63959533+deep-2008@users.noreply.github.com>
2022-05-21 10:38:05 +0530

    Time: 31 ms (38.26%), Space: 24 MB (26.54%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void floodfill(vector<vector<int>>& grid,int row,int col,int &sum,int &ans,vector<vector<int>>& vis){
+        
+        if(row<0 || col<0 || row==grid.size() || col==grid[0].size() || grid[row][col]==0 || vis[row][col]==1)
+            return;
+        
+        if(grid[row][col]==1)
+            sum++;
+      ans=max(ans,sum);
+        vis[row][col]=1;
+        //grid[row][col]=0;
+        
+        floodfill(grid,row-1,col,sum,ans,vis);
+        floodfill(grid,row,col-1,sum,ans,vis);
+        floodfill(grid,row+1,col,sum,ans,vis);
+        floodfill(grid,row,col+1,sum,ans,vis);
+        
+        
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int sum=0,ans=0;
+        int m=grid.size();
+        int n=grid[0].size();
+        
+        vector<vector<int>> vis(m,vector<int> (n,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                floodfill(grid,i,j,sum,ans,vis);
+               // ans=max(ans,sum);
+                sum=0;
+            }
+        }
+        
+        return ans;
+    }
+};
