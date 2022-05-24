c9f648beeefed95b9a95b71d9fcff9ba4ad7ecf8
deep-2008 <63959533+deep-2008@users.noreply.github.com>
2022-05-24 18:52:59 +0530

    Time: 5 ms (43.82%), Space: 7.8 MB (56.25%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,6 +1,6 @@
 #define vii vector<vector<int>> 
 #define vi vector<int>
-class Solution {
+/*class Solution {
 public:
     void helper(vi &nums,vii &ans,vi &vis,vi &ds,int ind){
         if(ds.size()==nums.size()){
@@ -28,6 +28,36 @@ public:
         vi ds;
         
         helper(nums,ans,vis,ds,0);
+        
+        return ans;
+    }
+};*/
+
+//OPTIMISED
+class Solution {
+public:
+    void helper(vi &nums,vii &ans,int ind){
+        if(ind==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[i],nums[ind]);
+            helper(nums,ans,ind+1);
+            swap(nums[i],nums[ind]);
+           
+        }
+        
+        
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+       vii ans;
+       // vi vis(nums.size(),0);
+     //   vi ds;
+        
+        helper(nums,ans,0);
         
         return ans;
     }
