ccc4e9e1d64bab3c06fd76d382bae0112a443b85
deep-2008 <63959533+deep-2008@users.noreply.github.com>
2022-05-24 09:18:41 +0530

    Time: 5 ms (56.62%), Space: 6.8 MB (96.04%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int left=0,right=0,maxl=0;
+        
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(')
+                left++;
+            else
+                right++;
+            
+            if(left==right)
+                maxl = max(maxl,left);
+            
+            else if(right>=left){
+                left=0;
+                right=0;
+            }
+        }
+        
+        left=0;right=0;
+        
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='(')
+                left++;
+            else
+                right++;
+            
+            if(left==right)
+                maxl=max(maxl,right);
+            
+            else if(left>=right){
+                right=0;
+                left=0;
+            }
+        }
+        
+        return maxl*2;
+    }
+};
