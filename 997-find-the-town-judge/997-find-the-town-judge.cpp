62dd32c3135e52306cb9b242a700c29fdc86d1e5
deep-2008 <63959533+deep-2008@users.noreply.github.com>
2022-06-01 09:27:27 +0530

    Time: 322 ms (16.80%), Space: 70.2 MB (13.06%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<vector<int>> adj(n+1),trans(n+1);
+        if(n==1)
+            return 1;
+        for(int i=0;i<trust.size();i++){
+            int u=trust[i][0];
+            int v=trust[i][1];
+            
+            adj[u].push_back(v);
+            trans[v].push_back(u);
+        }
+        
+        vector<int> indeg(n+1,0),outdeg(n+1,0);
+        
+        for(int i=0;i<n+1;i++){
+            for(auto it: adj[i]){
+                indeg[it]++;
+            }
+        }
+        
+        int judge=0;
+        
+        for(int i=0;i<n+1;i++){
+            if(indeg[i]==n-1 ){
+                judge= i;
+                break;
+            }
+        }
+        
+        for(int i=0;i<n+1;i++){
+            for(auto it: trans[i]){
+                outdeg[it]++;
+            }
+        }
+        
+        if(judge!=0 && outdeg[judge]==0)
+            return judge;
+        
+        
+        return -1;
+    }
+};
