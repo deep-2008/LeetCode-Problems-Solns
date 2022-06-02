704bbcdca024c94a2498d40ab844032b3ba97452
deep-2008 <63959533+deep-2008@users.noreply.github.com>
2022-06-02 21:52:33 +0530

    Time: 928 ms (25.10%), Space: 16 MB (40.62%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+       
+        unordered_set<string> list;
+        
+        for(auto i:wordList)
+            list.insert(i);
+        
+        if(list.find(endWord)==list.end())
+            return 0;
+        
+        queue<string> q;
+        
+        // set<string> vis;
+        // vis.insert(beginWord);
+        unordered_map<string,int> mp;
+        mp[beginWord]=1;
+        
+        q.push(beginWord);
+        
+        sort(wordList.begin(),wordList.end()); //For binary search
+        
+        int level=0;
+        
+        while(!q.empty()){
+            level++;
+            int size=q.size();
+            
+            while(size-- >0){
+                string node=q.front();
+                q.pop();
+                
+                if(node == endWord)
+                    return level;
+                
+                for(int i=0;i<node.size();i++){
+                    char old=node[i];
+                    
+                    for(char j='a';j<='z';j++){
+                        if(j!=old){
+                        node[i]=j;
+                        
+                        //if(list.find(node)!=list.end() && !vis.count(node)){
+                            if(binary_search(wordList.begin(),wordList.end(),node) && !mp[node]){
+                            q.push(node);
+                           //vis.insert(node);
+                           // list.erase(node);
+                                mp[node]=1;
+                        }
+                      }
+                    }
+                    node[i]=old;
+                }
+            }
+        }
+        
+        return 0;
+    }
+};
