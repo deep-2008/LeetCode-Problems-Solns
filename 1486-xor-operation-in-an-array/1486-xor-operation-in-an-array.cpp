d8d58299adff650afc5afdf580977c121283a0f5
deep-2008 <63959533+deep-2008@users.noreply.github.com>
2022-06-06 17:09:41 +0530

    Time: 4 ms (21.45%), Space: 5.8 MB (78.73%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        
+        int i=0;
+        int xor1=0;
+        while(i<n){
+            xor1 = xor1 ^ (start+(2*i));
+            i++;
+        }
+        
+        return xor1;
+    }
+};
