9858022b6d111b67e8e4469dfabee9afc834d1e6
deep-2008 <63959533+deep-2008@users.noreply.github.com>
2022-06-13 09:40:20 +0530

    Time: 11 ms (37.61%), Space: 8.6 MB (91.19%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-      //  int dp[n][n];
-       // dp[0][0]=triangle[0][0];
-        if(n==1)
-            return triangle[0][0];
+//         int dp[n][n];
+//        // dp[0][0]=triangle[0][0];
+//         if(n==1)
+//             return triangle[0][0];
         
-      /*  for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
-                if(i==0)
-                    dp[i][j]=triangle[i][j];
-                else{
-                    int s=triangle[i][j] + dp[i-1][j];
-                    int b=triangle[i][j];
-                    if(j-1>=0)
-                        b+=dp[i-1][j-1];
-                    else
-                        b=INT_MAX;
-                    dp[i][j]= min(s,b);
-                }
-            }
-        }*/
+//        for(int i=0;i<n;i++){
+//             for(int j=0;j<=i;j++){
+//                 if(i==0)
+//                     dp[i][j]=triangle[i][j];
+//                 else{
+//                     int s=triangle[i][j] + dp[i-1][j];
+//                     int b=triangle[i][j];
+//                     if(j-1>=0)
+//                         b+=dp[i-1][j-1];
+//                     else
+//                         b=INT_MAX;
+//                     dp[i][j]= min(s,b);
+//                 }
+//             }
+//         }
         
         
         int dp[n];
@@ -34,12 +34,42 @@ public:
             }
         }
         
-        return dp[0];
+       return dp[0];
         
-        /*int c=INT_MAX;
+//         int c=INT_MAX;
         
-        for(int i=0;i<n;i++)
-            c=min(c,dp[n-1][i]);
-        return c;*/
+//         for(int i=0;i<n;i++)
+//             c=min(c,dp[n-1][i]);
+//         return c;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
