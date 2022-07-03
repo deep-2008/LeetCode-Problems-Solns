78658c9e2ec89774f7dc6d9570ddad44197c36cb
Dipesh Sinha <63959533+deep-2008@users.noreply.github.com>
2022-07-03 17:16:29 +0530

    Time: 272 ms (20.67%), Space: 14.8 MB (88.49%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -4,25 +4,16 @@ public:
         int m=matrix.size();
         int n=matrix[0].size();
         
-        int r=0,c=0;
-        for(int i=0;i<m;i++){
-            if(matrix[i][n-1]>=target){
-                r=i;break;
-            }
-        }
-        
-//         for(int i=n-1;i>=0;i--){
-//             if(matrix[r][i]<=target){
-//                 c=i;break;
-//             }
-//         }
-        
-//         if(matrix[r][c]==target)
-//             return true;
-        
+        // int r=0,c=0;
+        // for(int i=0;i<m;i++){
+        //     if(matrix[i][n-1]>=target){
+        //         r=i;break;
+        //     }
+        // }
+          
         
         for(int i=n-1;i>=0;i--){
-        int l=r,h=m-1;
+        int l=0,h=m-1;
         
         while(l<=h){
             int mid=(l+h)/2;
@@ -34,28 +25,9 @@ public:
                 l=mid+1;
             else
                 h=mid-1;
+           }
         }
-        }
         
-//         if(matrix[r][h]>target)
-//             c=l;
-        
-//         else if(matrix[r][h]<target)
-//             c=h;
-        
-//         int low=r,high=m-1;
-        
-//         while(low<=high){
-//            int mid=(low+high)/2;
-            
-//             if(matrix[mid][c]==target)
-//                 return true;
-            
-//             else if(matrix[mid][c]<target)
-//                 l=mid+1;
-//             else
-//                 h=mid-1;
-//         }
         
         return false;
     }
