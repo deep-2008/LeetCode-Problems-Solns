5c8b07444d4c9822d047ff78e907b4cd1bb2f784
Dipesh Sinha <63959533+deep-2008@users.noreply.github.com>
2022-07-06 07:58:20 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,17 +1,17 @@
 class Solution {
-public:
-    int solve(int dp[],int n){
-        if(n==1)
-            return 1;
-        if(n==0)
-            return 0;
-        if(dp[n]!=-1)
-            return dp[n];
-        return dp[n]=solve(dp,n-1)+solve(dp,n-2);
+    int dp[31];
+   int solve(int n){
+        if(n<=1)
+            return n;
+       if(dp[n]!=-1)
+           return dp[n];
+        
+        return dp[n]=solve(n-1)+solve(n-2);
     }
+public:
     int fib(int n) {
-        int dp[n+1];
         memset(dp,-1,sizeof(dp));
-        return solve(dp,n);
+        
+        return (solve(n));
     }
 };
