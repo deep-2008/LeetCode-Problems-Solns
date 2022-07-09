addbbcfd38bad1c6c6e527625c9b0b482e7fc309
Dipesh Sinha <63959533+deep-2008@users.noreply.github.com>
2022-07-09 16:18:28 +0530

    Time: 726 ms (14.29%), Space: 49.2 MB (14.90%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,20 +1,87 @@
 class Solution {
+    int dp[10001];
+    
+    bool helper(vector<int> &nums,int ind,int n){
+        if(ind==n-1)
+            return true;
+        
+        if(dp[ind]!=-1)
+            return dp[ind];
+        
+        int maxJump=nums[ind];
+        
+        for(int i=1;i<=maxJump;i++){
+            if(helper(nums,ind+i,n))
+                return dp[ind]=true;
+        }
+        
+        return dp[ind]=false;
+    }
 public:
     bool canJump(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1)
-            return true;
-        if(nums[0]==0)
-            return false;
-        int reachable=0;
+        memset(dp,-1,sizeof(dp));
         
-        for(int i=0;i<n;i++){
-            if(i>reachable)
-                return false;
-            reachable=max(reachable,nums[i]+i);
-        }
-        return true;
-        
-        
+        return helper(nums,0,nums.size());
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     bool canJump(vector<int>& nums) {
+//         int n=nums.size();
+//         if(n==1)
+//             return true;
+//         if(nums[0]==0)
+//             return false;
+//         int reachable=0;
+        
+//         for(int i=0;i<n;i++){
+//             if(i>reachable)
+//                 return false;
+//             reachable=max(reachable,nums[i]+i);
+//         }
+//         return true;
+        
+        
+//     }
+// };
