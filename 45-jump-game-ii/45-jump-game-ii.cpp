abfb4230fa65d32a58661c5a5eb1fe7e2ef5799e
Dipesh Sinha <63959533+deep-2008@users.noreply.github.com>
2022-07-09 16:46:25 +0530

    Time: 215 ms (42.93%), Space: 17.7 MB (13.57%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,19 +1,94 @@
 class Solution {
+    int dp[10001];
+   long long int minJumps(vector<int>& nums,int ind,int &Jump,int n){
+        if(ind==n-1)
+            return 0;
+        
+        if(dp[ind]!=-1)
+            return dp[ind];
+        
+       long long int temp=INT_MAX;
+        
+        int maxJump=nums[ind];
+        
+        for(int i=1;i<=maxJump;i++){
+            // Jump++;
+            if(ind+i>=n)
+                break;
+            
+            temp=min(temp,(1ll)*1+minJumps(nums,ind+i,Jump,n));
+            // Jump--;
+        }
+        
+       dp[ind]=temp;
+        return temp;
+    }
 public:
     int jump(vector<int>& nums) {
-        if(nums.size()==1||nums[0]==0)
-            return 0;
-        int farthest=0,current=0,jumps=0;
-        
-        for(int i=0;i<nums.size()-1;i++){
-            farthest=max(farthest,nums[i]+i);
-            
-            if(i==current){
-                current=farthest;
-                jumps++;
-            }
-            
-        }
-        return jumps;
+      memset(dp,-1,sizeof(dp));
+        int Jump=0;
+        return minJumps(nums,0,Jump,nums.size());
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int jump(vector<int>& nums) {
+//         if(nums.size()==1||nums[0]==0)
+//             return 0;
+//         int farthest=0,current=0,jumps=0;
+        
+//         for(int i=0;i<nums.size()-1;i++){
+//             farthest=max(farthest,nums[i]+i);
+            
+//             if(i==current){
+//                 current=farthest;
+//                 jumps++;
+//             }
+            
+//         }
+//         return jumps;
+//     }
+// };
