b994c377b64d559a34a6c06fc646f4027dd061d6
Dipesh Sinha <63959533+deep-2008@users.noreply.github.com>
2022-07-11 17:55:44 +0530

    Time: 1469 ms (6.80%), Space: 60 MB (9.76%) - LeetHub

---
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestSubstring(string s, int k) {
+        unordered_map<char,int> mp;
+        
+        for(auto i:s)
+            mp[i]++;
+        
+        int ind=0;
+        
+        while(ind<s.size() && mp[s[ind]]>=k)
+            ind++;
+        
+        if(ind==s.size())
+            return s.size();
+        
+        int left=longestSubstring(s.substr(0,ind),k);
+        int right=longestSubstring(s.substr(ind+1),k);
+        
+        return max(left,right);
+    }
+};
