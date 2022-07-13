99848274b947c7c7abeaf40729b30f777e8d4ec1
Dipesh Sinha <63959533+deep-2008@users.noreply.github.com>
2022-07-13 19:34:00 +0530

    Time: 33 ms (8.01%), Space: 22.1 MB (87.34%) - LeetHub

--- 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.cpp
+++ 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.cpp
@@ -11,63 +11,36 @@
  */
 class Solution {
 public:
-//     vector<int> largestValues(TreeNode* root) {
-//         if(root==NULL)
-//             return {};
-//         vector<int> ans;
-//         queue<TreeNode*> q;
-//         q.push(root);
-        
-//         while(!q.empty()){
-//             int size=q.size();
-//             int maxi=INT_MIN;
-//             // vector<int> v;
-            
-//             while(size-->0){
-//                 TreeNode* node =q.front();
-//                 q.pop();
-                
-//                 maxi=max(maxi,node->val);
-//                 // v.push_back(root->val);
-                
-//                 if(root->left)
-//                     q.push(root->left);
-//                 if(root->right)
-//                     q.push(root->right);
-//             }
-//             // // sort(v.begin(),v.end());
-//             // int temp=0;
-//             // for(auto i:v)
-//             //     temp=max(temp,i);
-//             ans.push_back(maxi);
-//         }
-//         return ans;
-//     }
-    
     vector<int> largestValues(TreeNode* root) {
-    //bfs 
-    //level order traversal
-    vector<int>res;
-    if(root==NULL) return res;
-    queue<TreeNode*>q;
-    q.push(root);
-    
-    while(!q.empty()){
-        int mi=INT_MIN;
-        int x=q.size();
-        for(int i=0;i<x;i++){
-            if(q.front()->val>mi)
-                mi=q.front()->val;
-            if(q.front()->left)
-            q.push(q.front()->left);
+        if(root==NULL)
+            return {};
+        vector<int> ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int size=q.size();
+            int maxi=INT_MIN;
+            // vector<int> v;
             
-            if(q.front()->right)
-                q.push(q.front()->right);
-            q.pop();
+            while(size-->0){
+                TreeNode* node =q.front();
+                q.pop();
                 
+                maxi=max(maxi,node->val);
+                // v.push_back(root->val);
+                
+                if(node->left)
+                    q.push(node->left);
+                if(node->right)
+                    q.push(node->right);
+            }
+            // // sort(v.begin(),v.end());
+            // int temp=0;
+            // for(auto i:v)
+            //     temp=max(temp,i);
+            ans.push_back(maxi);
         }
-        res.push_back(mi);
+        return ans;
     }
-    return res;
-}
 };
