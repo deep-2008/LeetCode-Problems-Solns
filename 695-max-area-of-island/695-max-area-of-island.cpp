ee07913959a6170f1e085baaceb2ba6682564b5a
Dipesh Sinha <63959533+deep-2008@users.noreply.github.com>
2022-07-15 08:11:32 +0530

    Time: 39 ms (28.66%), Space: 23.2 MB (67.07%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     void floodfill(vector<vector<int>>& grid,int row,int col,int &sum,int &ans,vector<vector<int>>& vis){
         
@@ -27,6 +27,48 @@ public:
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 floodfill(grid,i,j,sum,ans,vis);
+               // ans=max(ans,sum);
+                sum=0;
+            }
+        }
+        
+        return ans;
+    }
+};*/
+
+
+//SPACE OPTIMISED
+
+class Solution {
+public:
+    void floodfill(vector<vector<int>>& grid,int row,int col,int &sum,int &ans){
+        
+        if(row<0 || col<0 || row==grid.size() || col==grid[0].size() || grid[row][col]==0)
+            return;
+        
+        if(grid[row][col]==1)
+            sum++;
+      ans=max(ans,sum);
+        //vis[row][col]=1;
+        grid[row][col]=0;
+        
+        floodfill(grid,row-1,col,sum,ans);
+        floodfill(grid,row,col-1,sum,ans);
+        floodfill(grid,row+1,col,sum,ans);
+        floodfill(grid,row,col+1,sum,ans);
+        
+        
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int sum=0,ans=0;
+        int m=grid.size();
+        int n=grid[0].size();
+        
+       // vector<vector<int>> vis(m,vector<int> (n,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]!=0)
+                floodfill(grid,i,j,sum,ans);
                // ans=max(ans,sum);
                 sum=0;
             }
