21abeb5d8d5a05ab341c9c731fd89b1fe4af0032
Dipesh Sinha <63959533+deep-2008@users.noreply.github.com>
2022-07-17 16:16:25 +0530

    Time: 71 ms (49.09%), Space: 17.9 MB (8.84%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -6,84 +6,24 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-// class Solution {
-// public:
-//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-//         auto hb=headB,ha=headA;
-        
-//         while(hb->next)
-//             hb=hb->next;
-        
-//         while(ha->next)
-//             ha=ha->next;
-        
-//         hb->next=headA;
-//         ha->next=headB;
-        
-//         ha=headA;
-//         hb=headB;
-        
-//         while(ha!=hb){
-//             if(ha->val==hb->val){
-//                 if(ha->next && ha->next->val==hb->next->val)
-//                     return ha;
-//             }
-//             ha=ha->next;
-//             hb=hb->next;
-//         }
-//         return NULL;
-//     }
-// };
-
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        vector<int> temp;
-        
         if(headA==NULL || headB==NULL)
             return NULL;
         
-        ListNode* a=headA,*d=headA,*b=headB,*c=headB;
+        unordered_map<ListNode*,int> mp;
         
-//        while(a->next!=NULL)
-//            a=a->next;
-        
-//         while(c->next!=NULL)
-//             c=c->next;
-        
-//         if(a!=c)
-//             return NULL;
-        
-//         a->next=b;
-//         c->next=d;
-        
-//         while(a->next!=NULL && c->next!=NULL){
-//             a=a->next;
-//             c=c->next;
-//         }
-        
-        while(a!=b){
-            if(a==NULL)
-                a=headB;
-            else
-                a=a->next;
-            
-            if(b==NULL)
-                b=headA;
-            else
-                b=b->next;
+        while(headA){
+            mp[headA]++;
+            headA=headA->next;
         }
         
-       return a;
-        
-        
+        while(headB){
+            if(mp.find(headB)!=mp.end())
+                return headB;
+            headB=headB->next;
+        }
+        return NULL;
     }
 };
