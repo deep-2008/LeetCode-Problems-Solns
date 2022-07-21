92615ace0d5dfef3779e76068cc3f4d9d8955369
Dipesh Sinha <63959533+deep-2008@users.noreply.github.com>
2022-07-21 12:08:01 +0530

    Time: 12 ms (28.46%), Space: 11.4 MB (58.98%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,31 +1,29 @@
 class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size(),bsp=-prices[0],ssp=0,csp=0,profit=0;
-        
-        if(n==1)
+    int dp[5001][2];
+    int solve(vector<int>& prices,int n,int ind,int buy){
+        if(ind>=n)
             return 0;
         
-      /*  for(int i=1;i<n;i++){
-            bsp=max(bsp,csp-prices[i]);
-            
-            ssp=max(ssp,bsp+prices[i]);
-            
-            csp=max(csp,ssp);
-        }*/
+        if(dp[ind][buy]!=-1)
+            return dp[ind][buy];
         
-        vector<vector<int>> dp(n,vector<int>(3,INT_MIN));
-        dp[0][0]=-prices[0];
-        dp[0][1]=0;
-        dp[0][2]=0;
+        int profit=0;
         
-        for(int i=1;i<n;i++){
-            dp[i][0]= max(dp[i-1][0],dp[i-1][2]-prices[i]); //buy state
-            
-            dp[i][1]= max(dp[i-1][1],dp[i-1][0]+prices[i]);//sell state
-            
-            dp[i][2]= max(dp[i-1][2],dp[i-1][1]); // cool down state
+        if(buy==1){
+            profit=max(-prices[ind]+solve(prices,n,ind+1,0),solve(prices,n,ind+1,1));
         }
-        return dp[n-1][1];
+        
+        else{
+            profit=max(prices[ind]+solve(prices,n,ind+2,1),solve(prices,n,ind+1,0));
+        }
+        
+        return dp[ind][buy]=profit;
+    }
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        memset(dp,-1,sizeof(dp));
+        
+        return solve(prices,n,0,1);
     }
 };
