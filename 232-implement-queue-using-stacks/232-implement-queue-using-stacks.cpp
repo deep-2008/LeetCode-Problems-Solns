b7ecaf53a9481f01c4d1d5e46b7c7103c73edde1
Dipesh Sinha <63959533+deep-2008@users.noreply.github.com>
2022-08-01 17:59:23 +0530

    Time: 10 ms (7.96%), Space: 7.1 MB (56.96%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -1,17 +1,22 @@
 class MyQueue {
-    stack<int> s1;
+    stack<int> s1,s2;
 public:
     MyQueue() {
         
     }
     
     void push(int x) {
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+        }
         s1.push(x);
         
-//         while(!s2.empty()){
-//             s1.push(s2.top());
-//             s2.pop();
-//         }
+        
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
+        }
         
 //         while(!s1.empty()){
 //             s2.push(s1.top());
@@ -19,31 +24,16 @@ public:
 //         }
     }
     
-    int pop() {
-       
-        stack<int> st;
-        while(!s1.empty()){
-            st.push(s1.top());
-            s1.pop();
-        }
+    int pop() {        
+        int x=s1.top();
+        s1.pop();
         
-        int x=st.top();
-        st.pop();
-        while(!st.empty()){
-            s1.push(st.top());
-            st.pop();
-        }
         return x;
     }
     
     int peek() {
-       stack<int> st,s=s1;
-        
-        while(!s.empty()){
-            st.push(s.top());
-            s.pop();
-        }
-        return st.top();
+       
+        return s1.top();
     }
     
     bool empty() {
