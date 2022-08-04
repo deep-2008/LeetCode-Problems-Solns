93eef9b4988ccc0eecbef37d48dd91828ad29b0f
Dipesh Sinha <63959533+deep-2008@users.noreply.github.com>
2022-08-04 09:24:50 +0530

    Time: 4 ms (23.97%), Space: 5.9 MB (26.45%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int mirrorReflection(int p, int q) {
+        int g=__gcd(p,q);
+        p=p/g;
+        q=q/g;
+        
+        if(p==q)
+            return 1;
+        
+        if(p%2==0)
+            return 2;
+        else{
+            if(q%2==0)
+                return 0;
+            else
+                return 1;
+        }
+    }
+};
