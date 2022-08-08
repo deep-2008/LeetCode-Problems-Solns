0bd69e6793692c2b97493ae007acf386a80098f2
Dipesh Sinha <63959533+deep-2008@users.noreply.github.com>
2022-08-08 08:23:16 +0530

    Time: 18 ms (82.58%), Space: 19.7 MB (81.75%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -3,20 +3,46 @@ public:
     int trap(vector<int>& height) {
         //PREF SUM
         
+//         int n=height.size();
+//         int ans=0,c=0,d=0;
+//         vector<int> pref(n,0),suff(n,0);
+        
+//         for(int i=0;i<n;i++){
+//              c=max(c,height[i]);
+//              d=max(d,height[n-i-1]);
+//             pref[i]=c;
+//             suff[n-i-1]=d;
+//         }
+        
+//         for(int i=0;i<n;i++){
+//             int c=min(pref[i],suff[i])-height[i];
+//             ans+=c;
+//         }
+        
+//         return ans;
+        
+        //USING TWO POINTERS
         int n=height.size();
-        int ans=0,c=0,d=0;
-        vector<int> pref(n,0),suff(n,0);
+        int ans=0,l=0,r=n-1,lmax=0,rmax=0;
         
-        for(int i=0;i<n;i++){
-             c=max(c,height[i]);
-             d=max(d,height[n-i-1]);
-            pref[i]=c;
-            suff[n-i-1]=d;
-        }
-        
-        for(int i=0;i<n;i++){
-            int c=min(pref[i],suff[i])-height[i];
-            ans+=c;
+        while(l<=r){
+            if(height[l]<=height[r]){
+                if(height[l]>=lmax)
+                    lmax=height[l];
+                
+                else
+                    ans+=(lmax-height[l]);
+                l++;
+            }
+            
+             else{
+                if(height[r]>=rmax)
+                    rmax=height[r];
+                
+                else
+                    ans+=(rmax-height[r]);
+                r--;
+            }
         }
         
         return ans;
