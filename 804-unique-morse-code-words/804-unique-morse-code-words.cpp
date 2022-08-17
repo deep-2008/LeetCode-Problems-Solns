0e5c5a3d408ee5b92fb3d2d0036aa24fb1d0649f
Dipesh Sinha <63959533+deep-2008@users.noreply.github.com>
2022-08-17 08:01:43 +0530

    Time: 8 ms (34.91%), Space: 8.8 MB (11.70%) - LeetHub

---
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        map<char,string> mp;
+        mp['a']=".-";
+        mp['b']="-...";
+        mp['c']="-.-.";
+        mp['d']="-..";
+        mp['e']=".";
+        mp['f']="..-.";
+        mp['g']="--.";
+        mp['h']="....";
+        mp['i']="..";
+        mp['j']=".---";
+        mp['k']="-.-";
+        mp['l']=".-..";
+        mp['m']="--";
+        mp['n']="-.";
+        mp['o']="---";
+        mp['p']=".--.";
+        mp['q']="--.-";
+        mp['r']=".-.";
+        mp['s']="...";
+        mp['t']="-";
+        mp['u']="..-";
+        mp['v']="...-";
+        mp['w']=".--";
+        mp['x']="-..-";
+        mp['y']="-.--";
+        mp['z']="--..";
+        // cout<<mp['a'];
+        // return 0;
+        
+        map<string,int> ans;
+        
+        for(int i=0;i<words.size();i++){
+            string temp="";
+            for(int j=0;j<words[i].size();j++){
+                temp+=mp[words[i][j]];
+            }
+            ans[temp]++;
+        }
+        return ans.size();
+    }
+};
